0893626fa4cd99b8b49e9033cef235948c0d31a9
kien.mac <kien.mac@niteco.se>
2023-06-19 14:55:32 +0700

    fix (security-risk): Remove mongodb credentials

--- api/src/config/db/index.js
+++ api/src/config/db/index.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 
 async function connect() {
-  console.log("process.env.MONGODB_URI",process.env.MONGODB_URI)
   try {
-    await mongoose.connect("mongodb://localhost:27017/K-Means");
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("connect db successfully");
   } catch (err) {
     console.log("connect db fail");
